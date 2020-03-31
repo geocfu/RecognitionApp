de890d89a208702a92ed14819fd1fddf4b25d6a1
George Mantellos <geocfu@tutamail.com>
2020-03-31 23:34:27 +0300

    Added a way to dynamicaly change theme

--- src/App.js
+++ src/App.js
@@ -1,22 +1,47 @@
 import 'react-native-gesture-handler';
 import React from 'react';
-import {StatusBar, StyleSheet} from 'react-native';
+import {StatusBar} from 'react-native';
 import {ApplicationProvider, IconRegistry} from '@ui-kitten/components';
 import {EvaIconsPack} from '@ui-kitten/eva-icons';
-import {mapping, light as theme} from '@eva-design/eva';
+import {mapping, light, dark} from '@eva-design/eva';
 import {AppNavigator} from './navigation/AppNavigator';
+import {ThemeContext} from './hooks/theme-context';
 
 // TODO: Save the theme
 
-const App = () => (
-  <React.Fragment>
-    <IconRegistry icons={EvaIconsPack} />
-    <ApplicationProvider mapping={mapping} theme={theme}>
-      {/* TODO: Need to change dynamically the color of the status bar */}
-      <StatusBar backgroundColor="#ffffff" barStyle="dark-content" />
-      <AppNavigator />
-    </ApplicationProvider>
-  </React.Fragment>
-);
+const appThemes = {light, dark};
+const statusBarColors = {light: '#FFFFFF', dark: '#222B45'};
+const statusBarStyles = {
+  light: 'dark-content',
+  dark: 'light-content',
+};
+
+const App = () => {
+  const [theme, setTheme] = React.useState('light');
+
+  const currentTheme = appThemes[theme];
+  const currentStatusBarColor = statusBarColors[theme];
+  const currentStatusBarStyle = statusBarStyles[theme];
+
+  const toggleTheme = () => {
+    const nextTheme = theme === 'light' ? 'dark' : 'light';
+    setTheme(nextTheme);
+  };
+
+  return (
+    <React.Fragment>
+      <IconRegistry icons={EvaIconsPack} />
+      <ThemeContext.Provider value={{theme, toggleTheme}}>
+        <ApplicationProvider mapping={mapping} theme={currentTheme}>
+          <StatusBar
+            backgroundColor={currentStatusBarColor}
+            barStyle={currentStatusBarStyle}
+          />
+          <AppNavigator />
+        </ApplicationProvider>
+      </ThemeContext.Provider>
+    </React.Fragment>
+  );
+};
 
 export default App;
