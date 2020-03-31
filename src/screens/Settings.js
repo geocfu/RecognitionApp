029201c0df71be48f7b27e115b0466dc51c81086
George Mantellos <geocfu@tutamail.com>
2020-04-01 02:54:27 +0300

    User can select a theme

--- src/screens/Settings.js
+++ src/screens/Settings.js
@@ -7,18 +7,41 @@ import {
   Text,
   TopNavigation,
   TopNavigationAction,
+  Button,
+  Radio,
+  RadioGroup,
 } from '@ui-kitten/components';
+
+import {ThemeContext} from '../hooks/theme-context';
 
 const BackIcon = style => <Icon {...style} name="arrow-back" />;
 
 export const Settings = ({navigation}) => {
-  const navigateBack = () => {
-    navigation.goBack();
-  };
+  const themeContext = React.useContext(ThemeContext);
+
+  const [selectedIndex, setSelectedIndex] = React.useState(0);
+
+  React.useEffect(() => {
+    if (themeContext.theme === 'light') {
+      setSelectedIndex(0);
+    } else {
+      setSelectedIndex(1);
+    }
+  }, [themeContext.theme]);
 
   const BackAction = () => (
-    <TopNavigationAction icon={BackIcon} onPress={navigateBack} />
+    <TopNavigationAction
+      icon={BackIcon}
+      onPress={() => {
+        navigation.goBack();
+      }}
+    />
   );
+
+  const onCheckedChange = index => {
+    setSelectedIndex(index);
+    themeContext.toggleTheme();
+  };
 
   return (
     <SafeAreaView style={{flex: 1}}>
@@ -28,8 +51,32 @@ export const Settings = ({navigation}) => {
         leftControl={BackAction()}
       />
       <Divider />
-      <Layout style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-        <Text category="h1">Settings</Text>
+      <Layout style={{flex: 1}} level="2">
+        <Text
+          category="h5"
+          style={{top: 10, left: 10, bottom: 10, right: 10, fontWeight: '700'}}>
+          Theme
+        </Text>
+        <RadioGroup
+          selectedIndex={selectedIndex}
+          onChange={onCheckedChange}
+          style={{top: 10, left: 15, bottom: 10, right: 10}}>
+          <Radio
+            style={{marginVertical: 8}}
+            textStyle={{fontSize: 16}}
+            text="Light Theme"
+          />
+          <Radio
+            style={{marginVertical: 8}}
+            textStyle={{fontSize: 16}}
+            text="Dark Theme"
+          />
+        </RadioGroup>
+        <Text
+          category="h5"
+          style={{top: 20, left: 10, bottom: 10, right: 10, fontWeight: '700'}}>
+          About
+        </Text>
       </Layout>
     </SafeAreaView>
   );
