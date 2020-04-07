f5430f091a211a805099107681e773eb8f45c0c6
George Mantellos <geocfu@tutamail.com>
2020-04-07 17:34:04 +0300

    New diagram introduced; line chart

---
+++ src/components/LineChartWithTheXAxisNamed.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import {Grid, LineChart, XAxis} from 'react-native-svg-charts';
+import {View} from 'react-native';
+
+export const LineChartWithTheXAxisNamed = props => {
+  const gridStrokeColor = props.theme == 'dark' ? '#FFF' : '#000';
+
+  return (
+    <View style={{height: 200, padding: 0, flexDirection: 'row'}}>
+      <View style={{flex: 1, marginLeft: 10}}>
+        <LineChart
+          style={{flex: 1}}
+          data={props.data}
+          contentInset={{top: 10, bottom: 10}}
+          svg={{stroke: '#3366FF'}}>
+          <Grid svg={{strokeWidth: 0.2, stroke: gridStrokeColor}} />
+        </LineChart>
+        <XAxis
+          style={{marginHorizontal: -10}}
+          data={props.data}
+          formatLabel={(value, index) => index}
+          contentInset={{left: 10, right: 10}}
+          svg={{fontSize: 10, fill: gridStrokeColor}}
+        />
+      </View>
+    </View>
+  );
+};
